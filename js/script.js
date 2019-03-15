245f529a318ebc02341f1dc064a45106a4a70f48
Nataly <nataly05olivares@gmail.com>
2019-03-15 01:24:29 +0000

    your message

--- js/script.js
+++ js/script.js
@@ -4,20 +4,22 @@
 /* global $ */
 
 $("#search-button").click(function(){
-    $('.gallery').empty();
+    $('gallery').empty();
     var searchTerm = $('#search-term').val();
     var url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
+   // var email = $("#email")val();
     $.ajax({
         url: url,
         method: "GET",
         success: function(response){
             var randIndex = Math.floor(Math.random() * response.data.length);
-            $('.gallery').append(
+            $('.gallery').html(
                 '<div class="col-md-offset-4 col-md-4 thumb">\
                     <a class="thumbnail" href =' + response.data[randIndex].images.fixed_width.url + '>\
                         <img src=' + response.data[randIndex].images.fixed_width.url + '/>\
                     </a>\
                 </div>'
+            
             );
         },
     }); 
